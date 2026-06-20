be6d077ae76691eb42c17b8a614cfc579fdd831a
Mayukh Bagchi <mayukhbagchi58@gmail.com>
2026-06-20 19:26:13 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> listVector = new HashMap<>();
+
+        for (String elem : strs) {
+            // 1. Convert string to char array
+            char[] charArray = elem.toCharArray();
+            // 2. Sort it (modifies in place)
+            Arrays.sort(charArray);
+            // 3. Convert back to string to use as a key
+            String tempStr = new String(charArray);
+
+            // 4. Use computeIfAbsent to initialize the list and add the word
+            if (!listVector.containsKey(tempStr)) {
+                // 2. If not, create a new list and put it in the map
+                listVector.put(tempStr, new ArrayList<String>());
+            }
+
+            listVector.get(tempStr).add(elem);
+        }
+
+        // Return the values (the groups) as a List
+        return new ArrayList<>(listVector.values());
+    }
+}
