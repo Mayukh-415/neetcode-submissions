945a68cfd2c962742fb59ba33b63d067d395187c
Mayukh Bagchi <mayukhbagchi58@gmail.com>
2026-06-24 08:51:47 +0530

    Add: top-k-elements-in-list - submission-3

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-3.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        
+        // int []arr = new int[10];
+
+        // for(int i=0; i<= nums.size(); i++) {
+        //     int elem = nums[i];
+        //     arr[i] += 1;
+        // }
+
+        int[] ans = new int[k];
+
+        HashMap<Integer, Integer>freqCounter = new HashMap<>();
+
+        for (int elem : nums) {
+            freqCounter.put(elem, freqCounter.getOrDefault(elem, 0)+1);
+        }
+
+        // List<Integer> topElements = freqCounter.entrySet().stream()
+        //         .sorted(Map.Entry.<Integer, Integer>comparingByValue().reversed()) // Sort by value, descending
+        //         .limit(k)                                                        // Take the top k
+        //         .map(Map.Entry::getKey)                                          // Extract the key
+        //         .collect(Collectors.toList());
+        int j = 0;
+        // for(int elem : topElements) {
+        //     ans[j++] = elem;
+        // }
+
+        PriorityQueue<Map.Entry<Integer, Integer>> minHeap = new PriorityQueue<>(Map.Entry.comparingByValue());
+
+        for(Map.Entry<Integer, Integer> entry : freqCounter.entrySet()) {
+            minHeap.add(entry);
+            if(minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+
+        while(!minHeap.isEmpty()) {
+            ans[j++] = minHeap.poll().getKey();
+        }
+
+
+
+        return ans;
+
+        
+    }
+}
